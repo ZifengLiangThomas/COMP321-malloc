41bddafa5005f961f375fe412c7ee4eef2a71442
Xin <xinhuangnyc@gmail.com>
2015-03-17 19:00:01 -0500

    team name

--- mm.c
+++ mm.c
@@ -27,7 +27,7 @@
  ********************************************************/
 team_t team = {
 	/* Team name */
-	"/* Team name */",
+	"/* Free() Phallic() */",
 	/* First member's full name */
 	"Xin Huang",
 	/* First member's email address */
